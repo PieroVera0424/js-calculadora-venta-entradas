057103ebe93fc29c8ce73e143aed35258c85d776
PieroVera0424 <pieroveraes@gmail.com>
2025-02-26 09:47:05 +0100

    java terminado

---
+++ Venta_De_Entradas/java.js
@@ -0,0 +1,24 @@
+document.getElementById("calculo").addEventListener("click", function() {
+    let edad = document.getElementById("edad").value;
+    let resultado = document.getElementById("resultado");
+    
+  
+    resultado.textContent = '';
+    if (edad === '' || isNaN(edad) || edad <= 0) {
+        resultado.textContent = 'Por favor, introduce una edad válida mayor que 0.';
+        resultado.style.color = 'red';
+    } else {
+     
+        edad = parseInt(edad);
+        if (edad < 12) {
+            resultado.textContent = 'La entrada es gratuita.';
+            resultado.style.color = 'green';
+        } else if (edad >= 12 && edad <= 18) {
+            resultado.textContent = 'La entrada tiene un precio reducido.';
+            resultado.style.color = 'orange';
+        } else {
+            resultado.textContent = 'El precio completo de la entrada es 20€.';
+            resultado.style.color = 'blue';
+        }
+    }
+});
